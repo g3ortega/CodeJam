d0e007a4556cbb31b9649701d5208022bb67d7fc
gustavorayo <gustavo.rayo@hotmail.es>
2013-04-23 17:58:39 -0500

    Update TicTacToe.java

--- src/CodeJam2013/QualificationRound/TicTacToe.java
+++ src/CodeJam2013/QualificationRound/TicTacToe.java
@@ -16,22 +16,6 @@ public class TicTacToe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char matriz[][]={{'X','X','X','X'},
-                         {'Y','.','.','.'},
-                         {'Y','0','.','.'},
-                         {'T','.','.','.'}};//X win
-        char matriz1[][]={{'Y','Y','Y','T'},
-                          {'.','.','.','.'},
-                          {'Y','Y','X','T'},
-                          {'.','.','.','.'}};//y win
-        char matriz2[][]={{'X','X','Y','T'},
-                          {'Y','Y','X','X'},
-                          {'X','Y','Y','Y'},
-                          {'X','X','Y','X'}};//Draw
-        char matriz3[][]={{'X','X','0','T'},
-                          {'X','.','.','.'},
-                          {'X','0','.','.'},
-                          {'Y','.','.','.'}};//Not finished
         Board tablero=new Board(matriz2);
         System.out.println(tablero.ObtenerEstado());
     }
